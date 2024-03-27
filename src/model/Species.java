48dcc01f0dd04b5c777eab4d306d3d825656deea
Simón García <157841549+SimonGarcia01@users.noreply.github.com>
2024-03-27 02:00:57 -0500

    Hadn't restarted the Species class. Done.

---
+++ src/model/Species.java
@@ -0,0 +1,14 @@
+package model;
+
+public class Species {
+    
+    //Attributes
+    private String name;
+    private String photo;
+    private int localPop;
+    
+    //Relations
+
+    //Methods
+
+}
